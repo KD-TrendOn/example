7b37208472287dbe9f972226e14672c940102757
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-10-10 04:56:26 +0300

    ew

--- main.py
+++ main.py
@@ -3,8 +3,44 @@ import os
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import asyncio
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
-llm = ChatOpenAI(temperature=1, model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"), openai_api_base="https://api.proxyapi.ru/openai/v1")
+def setup_logger(name):
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.NOTSET)
+
+    # Создаем обработчик для записи логов в файл
+    file_handler = RotatingFileHandler(
+        os.path.join(log_dir, f'{name}.log'),
+        maxBytes=10*1024*1024,  # 10MB
+        backupCount=5
+    )
+    file_handler.setLevel(logging.NOTSET)
+
+    # Создаем обработчик для вывода логов в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.NOTSET)
+
+    # Создаем форматтер
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Добавляем обработчики к логгеру
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger(__name__)
+
+llm = ChatOpenAI(temperature=1, model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"), openai_api_base="https://openrouter.ai/api/v1")
 prompt = ChatPromptTemplate.from_messages([
     ("system", "Answer to user question"),
     ("human", "{question}")
