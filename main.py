7552b7bbe75006d49169fa5239891359cc1d8f19
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-10-10 04:24:30 +0300

    now

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from langchain_openai import ChatOpenAI
 import os
 from langchain_core.output_parsers import StrOutputParser
-from langchain.prompts import ChatPromptTemplate
-import time
+from langchain_core.prompts import ChatPromptTemplate
+import asyncio
+
 llm = ChatOpenAI(temperature=1, model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"), openai_api_base="https://api.proxyapi.ru/openai/v1")
 prompt = ChatPromptTemplate.from_messages([
     ("system", "Answer to user question"),
     ("human", "{question}")
 ])
 chain = prompt | llm | StrOutputParser()
-for i in range(200):
-    time.sleep(3)
-    print(chain.ainvoke({"question":"Напиши пример программы на C++"}))
+
+async def main():
+    for i in range(200):
+        await asyncio.sleep(3)
+        result = await chain.ainvoke({"question": "Напиши пример программы на C++"})
+        print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
